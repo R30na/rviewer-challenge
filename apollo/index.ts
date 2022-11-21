985f805006c69e6a7d8b041540e6fdaeb89a9c26
Alireza Mehrsina <alireza.mehr30na@hiq.se>
2022-11-21 19:32:56 +0100

    fix api url

--- apollo/index.ts
+++ apollo/index.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const cache = new InMemoryCache();
 
 export const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: "/api/graphql",
   cache,
   connectToDevTools: true,
 });
